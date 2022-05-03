ff7acfdd4893f0d474af17542a8bd94943503306
Mark <mdixn1@gmail.com>
2022-05-03 13:42:39 -0400

    add dot env vars

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const https = require("https"); //included with Node
 const mailchimp = require("@mailchimp/mailchimp_marketing");
-const listId = "bdcf87f887";//MailChimp List, currently using my Main Audience
+require("dotenv").config();
 
-//app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.json()); //express now includes JSON parser
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public")); //use static "local" relative files
@@ -15,8 +15,9 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html" );
 });
 
+//Config Mailchimp
 mailchimp.setConfig({
-  apiKey: "a8ac082a0b5a2f3c1159497a98f8f0b1a-us1",
+  apiKey: "8ac082a0b5a2f3c1159497a98f8f0b1a-us1",
   server: "us1",
 });
 
@@ -39,16 +40,17 @@ app.post("/", function(req, res) { //"callback function"
   //Had to use Try+Catch for the error redirection
   async function run() {
     try {
-    const response = await mailchimp.lists.addListMember(listId, {
+    const response = await mailchimp.lists.addListMember(process.env.LISTID, {
       email_address: subscribingUser.email,
-      status: "subscribed",
+      status: "pending",
       merge_fields: {
         FNAME: subscribingUser.firstName,
         LNAME: subscribingUser.lastName
       }
     });
 
-      res.sendFile(__dirname + "/success.html");
+      res.sendFile(__dirname + "/success.html");      
+
     } catch (err) {
       console.log(err.status);
       res.sendFile(__dirname + "/failure.html");
@@ -66,5 +68,5 @@ app.post("/failure", function(req, res) {
 
 //Using dynamic PORT for Heroku OR Locally
 app.listen(process.env.PORT || 3000, function() {
-  //console.log("Server running on Port 3000");
+  console.log("Server running on Port 3000");
 });
